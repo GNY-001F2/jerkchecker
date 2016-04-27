80563a76f13fdc1081d5dfe7901d600cace6e3da
Aayush Agarwal <aayush.agarwal_wu@yahoo.com>
2016-04-27 06:23:29 -0400

    improved formatting, updated documentation, improved C++11 style loops and increased main() functionality

--- jerkchecker.cxx
+++ jerkchecker.cxx
@@ -11,6 +11,13 @@
 #define PREDEFINED_JERK 22445
 
 namespace jerkchecker {
+/* contains:
+ * vector<int> jerklist that stores the list of jerks
+ * void addJerk() add a jerk
+ * int isJerk() check if someone is a jerk
+ * vector<int> getJerkList() get a copy of the current
+ * list of jerks
+ */
 typedef struct jerk_t jerk_t;
 struct jerk_t {
     jerk_t();
@@ -22,35 +29,90 @@ public:
     std::vector<int> getJerkList();
 };
 
-jerk_t::jerk_t() : jerklist(0) {
+/* constructor, initializes the list of jerks as empty
+ * and then adds predefined jerks during run time
+ * If you need to add more jerks, change the definition of
+ * PREDEFINED_JERK to be an array of jerks, and overload
+ * addJerk() to one that works with a collection of userids
+ */
+jerk_t::jerk_t(): jerklist(0) {
     addJerk(PREDEFINED_JERK);
 }
+
+/* void jerk_t::addJerk(int userid): adds a jerk to the list if that jerk does
+ * not already exist in the list
+ * int userid: the id# of the jerk
+ *
+ * this function does not return any value
+ */
 void jerk_t::addJerk(int userid) {
     if(isJerk(userid) == 0) jerklist.push_back(userid);
     return;
 }
+
+/* std::vector<int> jerk_t::getJerkList(): when called, returns a copy
+ * of the list of jerks
+ * this function takes no arguments
+ * this function returns a value of type vector<int>
+ */
 std::vector<int> jerk_t::getJerkList() {
     return jerklist;
 }
+
+/* int jerk_t::isJerk(int userid): Checks if a user is a jerk
+ * int userid: the id# of the person you suspect to be a jerk
+ * the function returns 1 if the jerk is in the list,
+ * and 0 if not
+ */
 int jerk_t::isJerk(int userid) {
-    size_t jerkcount = jerklist.size();
-    for(size_t i = 0; i<jerkcount; i++) {
-        if(userid == jerklist[i]) return 1; //jerk is already in the list
-    }
+    for(const auto& i: jerklist) if(userid == i) return 1; 
+    // jerk is already in the list  ^^^^^^^^^
     return 0;
 }
 }
+
+/** WARNING: Please only use valid arguments. Invalid input is
+ * untested.
+ *
+ * main function to check if the program works correctly. prints
+ * the first number you enter as an argument, checks if the second
+ * one is in the list and adds three more jerks to the list before
+ * printing the list.
+ * argc: the number of arguments. it has to be exactly 3
+ * for the program to work, provided your arguments are not
+ * invalid.
+ * argv[0] is the name of the executable
+ * argv[1] is a string that is not the id# of a jerk
+ * argv[2] contains the id# of the jerk you want to add
+ * to your list
+ * 
+ * returns 0 on successful use, -1 on erroneous input
+ */
 int main (int argc, char **argv) {
     if(argc == 3) {
         std::cout<<argv[1]<<std::endl;
         jerkchecker::jerk_t obj;
-        if(obj.isJerk(std::stoi(argv[2])) )std::cout<<"Yep."<<std::endl;
+        if(obj.isJerk(std::stoi(argv[2]))) std::cout<<"Yep."<<std::endl;
         else std::cout<<"No."<<std::endl;
-    return 0;
+        obj.addJerk(50);
+        obj.addJerk(70);
+        obj.addJerk(99);
+        std::vector<int> jerklistcpy = obj.getJerkList();
+        std::cout<<"jerklist:"<<std::endl;
+        for(const auto& i: jerklistcpy) std::cout<<i<<std::endl;
+        return 0;
     }
+    //invalid number of arguments
+    return -1;
 }
 /* output:
  *
  * $ ./jerkchecker 278846 22445
+ * 278846
  * Yep.
+ * jerklist:
+ * 22445
+ * 50
+ * 70
+ * 99
  */
