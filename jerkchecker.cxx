09afddf18e28b16e7f2b288a9ea7db4345533ee1
Aayush Agarwal <aayush.agarwal_wu@yahoo.com>
2016-04-27 02:18:14 -0400

    initial local commit

---
+++ jerkchecker.cxx
@@ -0,0 +1,53 @@
+/** Licensed under GPLv2
+ * Copyright (C) 2016 duelistgamer
+ * Full text here: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ */
+
+#include<iostream>
+#include<vector>
+#define PREDEFINED_JERK 22445
+
+namespace jerkchecker {
+typedef struct jerk_t jerk_t;
+struct jerk_t {
+    jerk_t();
+private:
+    std::vector<int> jerklist;
+public:
+    void addJerk(int userid);
+    int isJerk(int userid);
+    std::vector<int> getJerkList();
+};
+
+jerk_t::jerk_t() : jerklist(0) {
+    addJerk(PREDEFINED_JERK);
+}
+void jerk_t::addJerk(int userid) {
+    if(isJerk(userid) == 0) jerklist.push_back(userid);
+    return;
+}
+std::vector<int> jerk_t::getJerkList() {
+    return jerklist;
+}
+int jerk_t::isJerk(int userid) {
+    size_t jerkcount = jerklist.size();
+    for(size_t i = 0; i<jerkcount; i++) {
+        if(userid == jerklist[i]) return 1; //jerk is already in the list
+    }
+    return 0;
+}
+}
+int main (int argc, char **argv) {
+    if(argc == 3) {
+        std::cout<<argv[1]<<std::endl;
+        jerkchecker::jerk_t obj;
+        if(obj.isJerk(std::stoi(argv[2])) )std::cout<<"Yep."<<std::endl;
+        else std::cout<<"No."<<std::endl;
+    return 0;
+    }
+}
+/* output:
+ *
+ * $ ./jerkchecker 278846 22445
+ * Yep.
+ */
